c0cb653d1015a9ab3fd924e127b22a7fedb7b58f
Mike Calvanese <mfcalvanese@gmail.com>
2018-07-14 19:13:42 -0400

    add rinkeby config to fcr-web

--- fcr-web/src/index.js
+++ fcr-web/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+require('dotenv').config()
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
--- fcr-web/src/socketWeb3.js
+++ fcr-web/src/socketWeb3.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3';
 
-const socketWeb3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
+const socketWeb3 = new Web3(new Web3.providers.WebsocketProvider(process.env.REACT_APP_SOCKET_WEB3_URL))
 
 export default socketWeb3;
