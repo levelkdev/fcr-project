9460e489c98f9d48e8b780ee3be71edc5f17cb8a
Mike Calvanese <mfcalvanese@gmail.com>
2018-07-20 06:59:48 +0200

    update to latest rinkeby deployment config

--- fcr-web/src/socketWeb3.js
+++ fcr-web/src/socketWeb3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 
-// const socketWeb3 = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/_ws'))
-const socketWeb3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
+const socketWeb3 = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/_ws'))
+// const socketWeb3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
 
 export default socketWeb3;
