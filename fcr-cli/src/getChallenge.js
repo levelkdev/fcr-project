1f95666ae500dbf8b97c13e8fc6575a6d26059c1
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-10 16:35:28 -0400

    remove fcr-cli, replace with https://github.com/levelkdev/fcr-cli

--- fcr-cli/src/getChallenge.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (fcr, listingHash) => {
-  const listing = await fcr.registry.getListing(listingHash)
-  if (parseInt(listing.challengeID) > 0) {
-    const challenge = await fcr.registry.getChallenge(listing.challengeID)
-    return challenge
-  }
-}
